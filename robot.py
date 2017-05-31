0913f6026d61f8f370f9a9e08fca7385c4e430c5
SERKAN BIRCAN <serkan@siniradam.net>
2017-06-01 00:08:21 +0300

    added notify command (not finished yet).

--- robot.py
+++ robot.py
@@ -4,9 +4,10 @@ import gettext
 
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (
-    Updater, CommandHandler,
+    Updater, CommandHandler, Job,
     RegexHandler, ConversationHandler
 )
+from telegram.ext.dispatcher import run_async
 
 from modules import config
 from modules.ticker import Ticker, Markets
@@ -20,6 +21,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger('CryptoCurrency Bot')
 logger.addHandler(logging.FileHandler('.log', encoding='utf-8'))
 
+JOBS = []
 LANGUAGE = 0
 PAIR, MARKET = range(2)
 
@@ -107,12 +109,14 @@ def market(bot, update):
         update.message.reply_text(_("Unsupported pair.", user.id))
 
     update.message.reply_text(
-        _("Price: {} \n"
+        _("@ {}\n"
+          "Price: {}\n"
           "Open: {}\n"
           "24h High: {}\n"
           "24h Low: {}\n"
           "24h Volume: {}\n"
-          "24h Change: {}", user.id).format(market_ticker['price'], 
+          "24h Change: {}", user.id).format(tickr.market.value,
+                                            market_ticker['price'], 
                                             market_ticker['open24h'],
                                             market_ticker['high24h'],
                                             market_ticker['low24h'],
@@ -129,6 +133,55 @@ def _(message, user_id):
         return message
 
     return translate(message)
+
+@run_async
+def notify_callback(bot, job):
+    market = job.context['market']
+    pair = job.context['pair']
+    condition = job.context['condition'].split('=')[0]
+    condition_price = float(job.context['condition'].split('=')[1])
+
+    tickr.market = Markets[market]
+    tickr.pair = pair
+    last_price = float(tickr.ticker()['price'].split(' ')[1].replace(',', '.'))
+
+    if condition == '>':
+        if last_price > condition_price:
+            bot.send_message(job.context['chat_id'], text='Alarm: Last price: %s' % last_price)
+    elif condition == '<':
+        if last_price < condition_price:
+            bot.send_message(job.context['chat_id'], text='Alarm: Last price: %s' % last_price)
+
+def notify(bot, update, args, job_queue, chat_data):
+    chat_id = update.message.chat_id
+
+    if args[0] == 'pop':
+        try:
+            _job = JOBS[-1]
+        except IndexError:
+            update.message.reply_text('You have no active alarm.')
+            return
+
+        _job.schedule_removal()
+        JOBS.pop()
+        update.message.reply_text('Alarm successfully removed !')
+        return
+
+    try:
+        arguments = {
+            'chat_id': chat_id, 
+            'market': args[0], 
+            'pair': args[1],
+            'condition': args[2]
+        }
+
+        job = job_queue.run_repeating(notify_callback, 10.0, context=arguments)
+        JOBS.append(job)
+
+        update.message.reply_text('I will notify you ! (:')
+
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /notify market pair >=value')
 
 def cancel(bot, update):
     user = update.message.from_user
@@ -166,8 +219,13 @@ def main():
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
+
     dp.add_handler(conv_handler)
     dp.add_handler(ticker_handler)
+    dp.add_handler(CommandHandler("notify", notify,
+                                  pass_args=True,
+                                  pass_job_queue=True,
+                                  pass_chat_data=True))
     dp.add_error_handler(error)
 
     updater.start_polling()
