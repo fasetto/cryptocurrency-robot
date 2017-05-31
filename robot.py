93ae440993a5031fd8b788166820c8e0aa7cba57
SERKAN BIRCAN <serkan@siniradam.net>
2017-05-31 21:35:06 +0300

    added robot.py

--- robot.py
+++ robot.py
@@ -4,20 +4,26 @@ import gettext
 
 from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import (
-    Updater, CommandHandler, MessageHandler, Filters,
+    Updater, CommandHandler,
     RegexHandler, ConversationHandler
 )
 
 from modules import config
+from modules.ticker import Ticker, Markets
 
 t = gettext.translation('robot', localedir='./locales', languages=['tr_TR'])
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    datefmt='%m-%d-%Y %I:%M:%S %p',
                     level=logging.INFO)
 
 logger = logging.getLogger('CryptoCurrency Bot')
+logger.addHandler(logging.FileHandler('.log', encoding='utf-8'))
 
-LANGUAGE = range(1)
+LANGUAGE = 0
+PAIR, MARKET = range(2)
+
+tickr = Ticker()
 
 def start(bot, update):
     user = update.message.from_user
@@ -25,19 +31,12 @@ def start(bot, update):
 
     update.message.reply_text(
         _("%s, choose your language.", user.id) % user.first_name,
-        reply_markup=ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True, one_time_keyboard=True)
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, 
+                                         resize_keyboard=True, 
+                                         one_time_keyboard=True)
     )
 
     return LANGUAGE
-
-def _(message, user_id):
-    translate = t.gettext
-    lang = config.language(user_id)
-
-    if lang == 'ENG':
-        return message
-
-    return translate(message)
 
 def language(bot, update):
     user = update.message.from_user
@@ -53,13 +52,93 @@ def language(bot, update):
         reply_markup=ReplyKeyboardRemove()
     )
 
-    return ConversationHandler.END
+    return -1
+
+def ticker(bot, update):
+    user = update.message.from_user
+    reply_keyboard = [['BTC/USD', 'BTC/EUR'],
+                      ['ETH/USD', 'ETH/BTC'],
+                      ['ETC/USD', 'ETC/BTC'],
+                      ['LTC/USD', 'LTC/BTC'],
+                      ['XRP/USD', 'XRP/BTC'],
+                      ['XMR/USD', 'XMR/BTC']]
+
+    update.message.reply_text(
+        _("Choose a pair.", user.id),
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, 
+                                         resize_keyboard=True, 
+                                         one_time_keyboard=True)
+    )
+
+    return PAIR
+
+def pair(bot, update):
+    user = update.message.from_user
+    couple = update.message.text
+    reply_keyboard = [['⬅️ Back'],
+                      ['Paribu', 'Koinim'],
+                      ['Bitstamp', 'Bitfinex'],
+                      ['Cexio', 'Poloniex'],
+                      ['Coinbase', 'Kraken'],
+                      ['BTCE', 'OKCoin']]
+
+    tickr.pair = couple
+
+    update.message.reply_text(
+        _("Now choose a market.", user.id),
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard,
+                                         resize_keyboard=True,
+                                         one_time_keyboard=True)
+    )
+
+    return MARKET
+
+def market(bot, update):
+    user = update.message.from_user
+    market = update.message.text
+
+    if market == '⬅️ Back':
+        return ticker(bot, update)
+
+    tickr.market = Markets[market]
+    market_ticker = tickr.ticker()
+
+    if market_ticker is None:
+        update.message.reply_text(_("Unsupported pair.", user.id))
+
+    update.message.reply_text(
+        _("Price: {} \n"
+          "Open: {}\n"
+          "24h High: {}\n"
+          "24h Low: {}\n"
+          "24h Volume: {}\n"
+          "24h Change: {}", user.id).format(market_ticker['price'], 
+                                            market_ticker['open24h'],
+                                            market_ticker['high24h'],
+                                            market_ticker['low24h'],
+                                            market_ticker['volume24h'],
+                                            market_ticker['change24h']),
+    )
+
+
+def _(message, user_id):
+    translate = t.gettext
+    lang = config.language(user_id)
+
+    if lang == 'ENG':
+        return message
+
+    return translate(message)
 
 def cancel(bot, update):
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
 
-    return ConversationHandler.END
+    update.message.reply_text(
+        _("", user.id),
+        reply_markup=ReplyKeyboardRemove()
+    )
+    return -1
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
@@ -75,11 +154,20 @@ def main():
         states={
             LANGUAGE: [RegexHandler('^(🇬🇧 English|🇹🇷 Türkçe)', language)]
         },
-
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
+    ticker_handler = ConversationHandler(
+        entry_points=[CommandHandler('ticker', ticker)],
+
+        states={
+            PAIR: [RegexHandler('^(BTC/USD|BTC/EUR|ETH/USD|ETH/BTC|ETC/USD|ETC/BTC|LTC/USD|LTC/BTC|XRP/USD|XRP/BTC|XMR/USD|XMR/BTC)', pair)],
+            MARKET: [RegexHandler('^(⬅️ Back|Paribu|Koinim|Bitstamp|Bitfinex|Cexio|Poloniex|Coinbase|Kraken|BTCE|OKCoin)', market)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
     dp.add_handler(conv_handler)
+    dp.add_handler(ticker_handler)
     dp.add_error_handler(error)
 
     updater.start_polling()
