f93a1e3568c910bcf3bc9ec91d9875d582067173
SERKAN BIRCAN <serkan@siniradam.net>
2017-06-01 14:42:15 +0300

    add support for Paribu.

--- modules/ticker.py
+++ modules/ticker.py
@@ -1,6 +1,9 @@
 
 from enum import Enum, unique
+import json
+import re
 import requests
+from bs4 import BeautifulSoup
 
 BASE_URL = "https://min-api.cryptocompare.com/data/generateAvg?fsym=%s&tsym=%s&markets=%s"
 KOIMIM_URL = "https://koinim.com/ticker/"
@@ -48,7 +51,7 @@ class Ticker:
         result = self.__api_call(request_url)
 
         try:
-            if self.market == Markets.Koinim:
+            if self.market == Markets.Koinim and self.pair == 'BTC/TRY':
                 result = {
                     'price': "₺ {:,.2f}".format(result['last_order']),
                     'volume24h': "Ƀ {:,.2f}".format(result['volume']),
@@ -58,6 +61,8 @@ class Ticker:
                     'change24h': "% {:.2f}".format(result['change_rate'])
                 }
                 return result
+            elif self.market == Markets.Paribu and self.pair == 'BTC/TRY':
+                return self.paribu_ticker()
 
             result = {
                 'price': result['DISPLAY']['PRICE'],
@@ -67,9 +72,30 @@ class Ticker:
                 'low24h': result['DISPLAY']['LOW24HOUR'],
                 'change24h': result['DISPLAY']['CHANGE24HOUR']
             }
+
+
+
         except KeyError:
             return None
 
+        return result
+
+    def paribu_ticker(self):
+        html_doc = requests.get('https://www.paribu.com').text
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        js_text = soup.find('script', type="text/javascript").text
+        regex =  re.compile('("day_stats"):(.*?)$|,', re.MULTILINE)
+        js_text = re.findall(regex, js_text)[7][1]
+        result = json.loads(re.sub("}}};", "", js_text))
+
+        result = {
+            'price': "₺ {:,.2f}".format(result['lst']),
+            'volume24h': "NaN",
+            'open24h': 'NaN',
+            'high24h': "₺ {:,.2f}".format(result['max']),
+            'low24h': "₺ {:,.2f}".format(result['min']),
+            'change24h': "₺ {:.2f}".format(result['chg'])
+        }
         return result
 
     def __api_call(self, uri):
